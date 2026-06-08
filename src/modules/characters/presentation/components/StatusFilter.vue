2cc97456963ccd48fefac16c2e66c886b2f1f196
Vinicius Nascimento <vinninascimento1305@gmail.com>
2026-06-08 17:45:28 -0300

    fix: corrige integração do filtro de status

--- src/modules/characters/presentation/components/StatusFilter.vue
+++ src/modules/characters/presentation/components/StatusFilter.vue
@@ -1,92 +1,95 @@
 <script setup>
 defineProps({
-  currentStatus: String
+  currentStatus: {
+    type: String,
+    default: ''
+  }
 })
 
 const emit = defineEmits(['change'])
+
+const statuses = [
+  {
+    label: 'Todos',
+    value: ''
+  },
+  {
+    label: 'Alive',
+    value: 'alive'
+  },
+  {
+    label: 'Dead',
+    value: 'dead'
+  },
+  {
+    label: 'Unknown',
+    value: 'unknown'
+  }
+]
+
+const selectStatus = (status) => {
+  emit('change', status)
+}
 </script>
 
 <template>
-  <div class="filters">
+  <div class="status-filter">
     <button
-      :class="{ active: currentStatus === '' }"
-      @click="emit('change', '')"
+      v-for="status in statuses"
+      :key="status.value"
+      class="status-btn"
+      :class="{
+        active: currentStatus === status.value
+      }"
+      @click="selectStatus(status.value)"
     >
-      Todos
-    </button>
-
-    <button
-      :class="{ active: currentStatus === 'alive' }"
-      @click="emit('change', 'alive')"
-    >
-      Alive
-    </button>
-
-    <button
-      :class="{ active: currentStatus === 'dead' }"
-      @click="emit('change', 'dead')"
-    >
-      Dead
-    </button>
-
-    <button
-      :class="{ active: currentStatus === 'unknown' }"
-      @click="emit('change', 'unknown')"
-    >
-      Unknown
+      {{ status.label }}
     </button>
   </div>
 </template>
 
 <style scoped lang="scss">
-.filters {
+.status-filter {
   display: flex;
   gap: 16px;
+}
 
-  button {
-    height: 56px;
+.status-btn {
+  height: 58px;
 
-    padding: 0 28px;
+  padding: 0 28px;
 
-    border: none;
+  border: none;
 
-    border-radius: 14px;
+  border-radius: 14px;
 
-    background: #111;
+  background: #111;
 
-    color: white;
+  color: white;
 
-    font-size: 18px;
-    font-weight: 800;
+  font-size: 20px;
+  font-weight: 800;
 
-    cursor: pointer;
+  cursor: pointer;
 
-    transition: all 0.25s ease;
-
-    box-shadow:
-      0 5px 0 #000,
-      0 8px 16px rgba(0,0,0,0.25);
-
-    &:hover {
-      transform: translateY(-3px);
-
-      box-shadow:
-        0 8px 0 #000,
-        0 12px 20px rgba(0,0,0,0.3);
-    }
-
-    &.active {
-  background: #d7ff39 !important;
-
-  color: #111 !important;
-
-  transform: translateY(-2px);
+  transition: .25s;
 
   box-shadow:
-    0 5px 0 #8dad00,
-    0 0 10px rgba(215,255,57,0.7),
-    0 0 20px rgba(215,255,57,0.5);
-}
+    0 6px 0 #000,
+    0 10px 16px rgba(0,0,0,.25);
+
+  &:hover {
+    transform: translateY(-2px);
   }
+}
+
+.active {
+  background: #d7ff39;
+
+  color: #111;
+
+  box-shadow:
+    0 4px 0 #9dc700,
+    0 0 18px rgba(215,255,57,.8);
 }
 </style>
